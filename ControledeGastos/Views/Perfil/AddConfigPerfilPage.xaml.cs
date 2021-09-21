82f45cc524cef81db0207948dd5a73a5d3bfb85a
Andryw Afonso <32003079+AndrywLino@users.noreply.github.com>
2021-09-21 13:50:37 -0300

    Revert "Revert "Perfil""
    This reverts commit cac2522586dabb05ca85cd5e97e7d7d5bc1e3d43.

--- ControledeGastos/Views/Perfil/AddConfigPerfilPage.xaml.cs
+++ ControledeGastos/Views/Perfil/AddConfigPerfilPage.xaml.cs
@@ -9,6 +9,7 @@ namespace ControledeGastos.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddConfigPerfilPage : ContentPage
     {
+        private int _page = 1;
         public AddConfigPerfilPage()
         {
             InitializeComponent();
@@ -16,6 +17,11 @@ namespace ControledeGastos.Views
         }
 
         private void BtnAddEntryValor(object sender, EventArgs e)
+        {
+            CriarValor();
+        }
+
+        private void CriarValor()
         {
             var newEntry = new Entry
             {
@@ -33,13 +39,27 @@ namespace ControledeGastos.Views
             {
                 Text = "Gostaria de ser notificado do saldo em alguma data especifica?"
             };
-            StacValor.Children.Add(newEntry);
-            StacValor.Children.Add(newLabel);
-            StacValor.Children.Add(newRadioSim);
-            StacValor.Children.Add(newRadioNao);
+            var newLabel1 = new Label
+            {
+                Text = "Informe a Data."
+            };
+            var newDate = new DatePicker
+            {
+                Date = DateTime.Now,
+            };
+            StackValor.Children.Add(newEntry);
+            StackValor.Children.Add(newRadioSim);
+            StackValor.Children.Add(newRadioNao);
+            StackValor.Children.Add(newLabel1);
+            StackValor.Children.Add(newDate);
         }
 
         private void BtnAddEntryCartao(object sender, EventArgs e)
+        {
+            CriarCartao();
+        }
+
+        private void CriarCartao()
         {
             var newEntry = new Entry
             {
@@ -49,19 +69,101 @@ namespace ControledeGastos.Views
             {
                 Date = DateTime.Now,
             };
-            StacCartao.Children.Add(newEntry);
-            StacCartao.Children.Add(newDate);
+            var newRadioSim = new RadioButton
+            {
+                Content = "Sim",
+            };
+            var newRadioNao = new RadioButton
+            {
+                Content = "Não",
+            };
+            var newLabel = new Label
+            {
+                Text = "Gostaria de ser notificado do vencimento?"
+            };
+            var newLabel1 = new Label
+            {
+                Text = "Qual a data de vencimento do cartão?"
+            };
+            StackCartao.Children.Add(newEntry);
+            StackCartao.Children.Add(newLabel1);
+            StackCartao.Children.Add(newDate);
+            StackCartao.Children.Add(newLabel);
+            StackValor.Children.Add(newRadioSim);
+            StackValor.Children.Add(newRadioNao);
         }
 
         private void BtnAddSalvar(object sender, EventArgs e)
         {
-            foreach (var item in StacValor.Children)
+            foreach (var item in StackValor.Children)
             {
                 if (item.GetType() == typeof(Entry))
                 {
                     Entry entry = (Entry)item;
                     var teste = entry.Text;
                 }
+            }
+        }
+
+        private void BtnProximo(object sender, EventArgs e)
+        {
+            if (_page == 1)
+            {
+                //Page 2
+                StackNomes.IsVisible = false;
+                StackCartao.IsVisible = true;
+                StackValor.IsVisible = false;
+                BtnProximoName.IsVisible = true;
+                BtnCartaoName.IsVisible = true;
+                BtnValorName.IsVisible = false;
+                BtnSalvarName.IsVisible = false;
+                BtnVoltarName.IsVisible = true;
+                CriarCartao();
+                _page++;
+            }
+            else if (_page == 2)
+            {
+                //Page 3
+                StackNomes.IsVisible = false;
+                StackCartao.IsVisible = false;
+                StackValor.IsVisible = true;
+                BtnProximoName.IsVisible = false;
+                BtnCartaoName.IsVisible = false;
+                BtnValorName.IsVisible = true;
+                BtnSalvarName.IsVisible = true;
+                BtnVoltarName.IsVisible = true;
+                CriarValor();
+                _page++;
+            }
+        }
+
+        private void BtnVoltar(object sender, EventArgs e)
+        {
+            if (_page == 2)
+            {
+                //Page 1
+                StackNomes.IsVisible = true;
+                StackCartao.IsVisible = false;
+                StackValor.IsVisible = false;
+                BtnProximoName.IsVisible = true;
+                BtnCartaoName.IsVisible = false;
+                BtnValorName.IsVisible = false;
+                BtnSalvarName.IsVisible = false;
+                BtnVoltarName.IsVisible = false;
+                _page--;
+            }
+            else if (_page == 3)
+            {
+                //Page 2
+                StackNomes.IsVisible = false;
+                StackCartao.IsVisible = true;
+                StackValor.IsVisible = false;
+                BtnProximoName.IsVisible = true;
+                BtnCartaoName.IsVisible = true;
+                BtnValorName.IsVisible = false;
+                BtnSalvarName.IsVisible = false;
+                BtnVoltarName.IsVisible = true;
+                _page--;
             }
         }
     }
